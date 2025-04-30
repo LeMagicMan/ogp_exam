303af890cb6a6e555a11cbd264f28abe3f84ed07
Ben Demets <ben.demets@student.kuleuven.be>
2025-04-30 13:30:17 +0200

    workflowfix

--- tests/InitialTest.java
+++ tests/InitialTest.java
@@ -1,11 +1,10 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class test {
+public class InitialTest {
 
     Boolean test;
 
